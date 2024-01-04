969d8827c0f92906f734fc053a3360bb9b30b5e3
Faustine67 <michelfaustine@gmail.com>
2024-01-04 16:33:56 +0100

    Architecture Projet

---
+++ project-root/app/Controllers/PostController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Model\PostModel;
+
+class PostController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
---
+++ project-root/app/Controllers/UserController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Model\UserModel;
+
+class UserController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
