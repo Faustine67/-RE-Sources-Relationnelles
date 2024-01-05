3268a84e1d56c95c5ddc771afc0f280083cba29c
Green-Blind <raoul.kirchhofer@gmail.com>
2024-01-05 16:46:47 +0100

    API Youtube
    Contrôleur + Vues + Routes

--- project-root/app/Config/Routes.php
+++ project-root/app/Config/Routes.php
@@ -7,4 +7,11 @@ use CodeIgniter\Router\RouteCollection;
  */
 $routes->get('/', 'Home::index');
 
+<<<<<<< Updated upstream
 $routes->get('video_details/', 'APIYoutube::index');
+=======
+$routes->get('liste_post', 'PostController::index');
+
+$routes->get('youtube/', 'VideoController::index');
+$routes->post('results_youtube/', 'VideoController::search');
+>>>>>>> Stashed changes
