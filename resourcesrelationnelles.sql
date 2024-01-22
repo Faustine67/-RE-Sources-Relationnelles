efe40b42f337d8e15d95313ac9917f1877b276fa
Green-Blind <raoul.kirchhofer@gmail.com>
2024-01-22 11:24:47 +0100

    Rename + Modif .sql

--- resourcesrelationnelles.sql
+++ resourcesrelationnelles.sql
@@ -1,39 +1,40 @@
+-- Table vidéo
 CREATE TABLE video(
-   youtube_id INT,
-   url TEXT,
-   title VARCHAR(256),
-   publicationDate VARCHAR(50),
-   PRIMARY KEY(youtube_id)
+   youtube_id INT PRIMARY KEY,
+   title VARCHAR(255),
+   thumbnails_url TEXT,
+   publicationDate VARCHAR(50)
 );
 
+-- Table utilisateur
 CREATE TABLE user(
-   id_user INT,
+   id_user INT AUTO_INCREMENT PRIMARY KEY,
    firstname VARCHAR(100),
    lastname VARCHAR(100),
    password VARCHAR(255),
-   email VARCHAR(255),
-   PRIMARY KEY(id_user)
+   email VARCHAR(255)
 );
 
+-- Table post
 CREATE TABLE post(
-   id_post INT,
+   id_post INT AUTO_INCREMENT PRIMARY KEY,
    text TEXT,
    id_user INT NOT NULL,
-   PRIMARY KEY(id_post),
    FOREIGN KEY(id_user) REFERENCES user(id_user)
 );
 
+-- Table commentaire
 CREATE TABLE comment(
-   id_comment INT,
+   id_comment INT AUTO_INCREMENT PRIMARY KEY,
    text TEXT,
    sendingDate DATETIME,
    id_user INT NOT NULL,
    id_post INT NOT NULL,
-   PRIMARY KEY(id_comment),
    FOREIGN KEY(id_user) REFERENCES user(id_user),
    FOREIGN KEY(id_post) REFERENCES post(id_post)
 );
 
+-- Table associative entre video et post
 CREATE TABLE video_id_post_id(
    youtube_id INT,
    id_post INT,
@@ -41,4 +42,3 @@ CREATE TABLE video_id_post_id(
    FOREIGN KEY(youtube_id) REFERENCES video(youtube_id),
    FOREIGN KEY(id_post) REFERENCES post(id_post)
 );
-
